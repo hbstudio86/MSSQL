b7b8c4669318f2f4cc3fe41cb283cbebcc8e1674
hbstudio <hbstudio@naver.com>
2024-02-10 16:28:22 +0900

    where 사용 및 like, order by 다시 연습

--- practice.sql
+++ practice.sql
@@ -1,6 +1,9 @@
 SELECT * FROM nasdaq_company
 --WHERE (ipo_year >= 1960 AND ipo_year <= 2010)
-WHERE ipo_year BETWEEN 1960 AND 2010
-AND symbol LIKE 'A%'
-AND is_deleted NOT LIKE 'null'
-ORDER BY ipo_year, sector, company_name,open_price;
+--WHERE ipo_year BETWEEN 1960 AND 2010
+--AND symbol LIKE 'A%'
+--AND is_deleted NOT LIKE 'null'
+--ORDER BY ipo_year, sector, company_name,open_price;
+-- WHERE ipo_year = 1970 OR sector != 'NULL';
+-- WHERE ipo_year IN(1970, 1980, 1990);
+WHERE ipo_year = 1970 OR ipo_year = 1980 OR ipo_year = 1990;
